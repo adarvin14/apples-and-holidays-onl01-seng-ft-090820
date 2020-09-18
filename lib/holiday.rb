b3f9f5eeff5a5c03355a1314f7af0cd5d15537e8
Alex Darvin <amdarvin14@gmail.com>
2020-09-18 16:44:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, holiday|
     holiday.map do |hoilday, supply|
-      holiday if suppl
+      holiday if supply.include?("BBQ")
+        
 end
 
 
